b6318936d91f284a8e7075eccb25e322081ba7be
hvlopez <hectorvmlopez@gmail.com>
2022-08-01 20:28:59 -0500

    adding menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import styled from "styled-components";
 import "./App.css";
 import Menu from "./components/menu";
@@ -6,11 +7,13 @@ import Navbar from "./components/navbar";
 function App() {
   return (
     <Container className="App">
-      <Menu />
-      <Main>
-        <Navbar />
-        <Wrapper>Video Cards</Wrapper>
-      </Main>
+      <BrowserRouter>
+        <Menu />
+        <Main>
+          <Navbar />
+          <Wrapper>Video Cards</Wrapper>
+        </Main>
+      </BrowserRouter>
     </Container>
   );
 }
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,6 +1,22 @@
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import HomeIcon from "@mui/icons-material/Home";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
 import { FC } from "react";
 import styled from "styled-components";
 import hectortube from "../img/logo.png";
+import {Link} from "react-router-dom";
 interface IMenu {}
 
 const Menu: FC<IMenu> = () => {
@@ -11,6 +27,76 @@ const Menu: FC<IMenu> = () => {
           <Image src={hectortube} />
           HectorTube
         </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlinedIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlinedIcon />
+          Subscriptions
+        </Item>
+
+        <Item>
+          <VideoLibraryOutlinedIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlinedIcon />
+          History
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comment, and subscribe.
+          <Link to="signin" style={{ textDecoration: "none" }}>
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+          </Link>
+        </Login>
+        <Hr />
+        <Title>BEST OF LAMATUBE</Title>
+        <Item>
+          <LibraryMusicOutlinedIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon />
+          Gaming
+        </Item>
+        <Item>
+          <MovieOutlinedIcon />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlinedIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlinedIcon />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlinedIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon />
+          Help
+        </Item>
       </Wrapper>
     </Container>
   );
@@ -20,7 +106,7 @@ export default Menu;
 const Container = styled.div`
   flex: 1;
   background-color: #202020;
-  height: 100vh;
+  height: 100%;
   color: white;
   font-size: 14px;
 `;
@@ -39,4 +125,39 @@ const Logo = styled.div`
 
 const Image = styled.img`
   height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0px;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid #373737;
+`;
+
+const Login = styled.div``;
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Title = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: #aaaaaa;
+  margin-bottom: 20px;
 `;
